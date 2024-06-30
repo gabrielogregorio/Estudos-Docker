7321738ecbef73662ca84a680772debd3ae03bff
Gabriel Gregorio <gabriel.gregorio.1@outlook.com>
2024-06-30 13:52:52 -0300

    fix: list users

--- src/listUser.js
+++ src/listUser.js
@@ -4,13 +4,12 @@ function listUsers() {
   return new Promise((resolve, reject) => {
     connection.query("SELECT name FROM users", (error, results) => {
       if (error) {
-        connection.end();
         return reject(error);
       }
       const userList = results.map((row) => row.name);
-      connection.end();
+      
       resolve(userList);
-    });
+    })
   });
 }
 
